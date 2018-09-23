09b02552d8c98214df3d5c40dc69a1e9a6aa8b5f
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-23 17:21:04 +0300

    Fixed the solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 module.exports = function getZerosCount(number, base) {
-  var zeros = 0;
-  while ( 1 <= number <= 10000000 || 2 <= base <= 256) {
-    number = Math.floor(number / 5);
-    zeros += number
-  }
-  return zeros;
+    var baseArray = [], n = 2, z = 0;
+    while ((n <= base) || (n <= base)) { 
+        if (base % n === 0) {
+            baseArray = base;
+            base = Math.round(base / n);
+        } else {
+            n++}
+    }
+    for (var i = 1; i < 12; i++) { 
+        var numbers;
+        numbers = Math.floor(number / Math.pow(baseArray, i));
+        z += numbers;} 
+return z;
 }
